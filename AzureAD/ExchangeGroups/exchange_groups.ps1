69f3f31fc9267f2dd8f880bbc549131bfcd3eed4
bsec676 <77978186+bsec676@users.noreply.github.com>
2021-07-26 13:53:21 -0500

    Script for adding new groups and users
    Create the group names GroupName with a Display Name of "Group Name" and an email address of "userPrincipalName@company.com" with alias of userPrincipalName Allow users from outside the org to send emails and then add these members to the group. This script can be setup a bit better by using a csv file to import all the attributes

---
+++ AzureAD/ExchangeGroups/exchange_groups.ps1
@@ -0,0 +1,10 @@
+﻿#Create Group
+
+#Connect to Exchange Online
+Import-Module ExchangeOnlineManagement
+$cred = Get-Credential 
+Connect-ExchangeOnline -Credential $cred
+
+#Create the group names GroupName with a Display Name of "Group Name" and an email address of "userPrincipalName@company.com" with alias of userPrincipalName Allow users from outside the org to send emails and then add these members to the group
+#This script can be setup a bit better by using a csv file to import all the attributes
+New-DistributionGroup -Type Distribution -Name "GroupName" -DisplayName "Group Name" -PrimarySmtpAddress "userPrincipalName@company.com" -Alias "userPrincipalName" -RequireSenderAuthenticationEnabled $false -Members user1@company.com,user2@company.com,user3@company.com,user4@company.com
