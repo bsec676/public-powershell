5722190ade0293113e9b1947a9ab236094aadd62
bsec676 <77978186+bsec676@users.noreply.github.com>
2021-07-26 13:52:37 -0500

    Set Devices to Disabled that are older than the date
    Set Devices to Disabled that are older than the date

---
+++ azure_computers/stale_computers_cleanup.ps1
@@ -0,0 +1,16 @@
+﻿#connect to AzureAD
+$AZCreds = Get-Credential
+Connect-AzureAD -Credential $AZCreds
+
+
+#Set Devices to Disabled that are older than the date
+$dt = [datetime]’2020/01/01’
+Get-AzureADDevice -All:$true | Where {$_.ApproximateLastLogonTimeStamp -le $dt} | Set-AzureADDevice -AccountEnabled:$False
+
+#Set-AzureADDevice -ObjectId "IdOfDevice" -AccountEnabled:$False
+
+#Get All Devices and sort by Displayname
+#Get-AzureADDevice -All:$true | Sort -property DisplayName
+
+#Get a specific Device 
+#Get-AzureADDeviceRegisteredOwner -ObjectId "IDOfDevice"
