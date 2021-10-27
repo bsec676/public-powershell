e721d72e509924c94bae348eb4afcd91d12a960d
bsec676 <77978186+bsec676@users.noreply.github.com>
2021-10-27 11:43:35 -0500

    initial commit

---
+++ MFA/MFA Status.ps1
@@ -0,0 +1,24 @@
+﻿Get-ExecutionPolicy
+Set-ExecutionPolicy Unrestricted –Scope CurrentUser
+
+$UserCredential = Get-Credential
+Import-Module MSOnline
+Connect-MsolService –Credential $UserCredential
+ 
+Get-MsolUser -all | select DisplayName,UserPrincipalName,@{
+N="MFA Status"; E={ if( $_.StrongAuthenticationRequirements.State -ne $null)
+{ $_.StrongAuthenticationRequirements.State} else 
+{ "Disabled"
+}
+}
+},Department, UserType, ObjectId, Manager | Sort-Object "MFA Status", DisplayName | Export-Csv "mfaexport.csv"
+
+
+get-msoluser -UserprincipalName upn@company.com -properties *
+
+Set-AzureADUserManager -ObjectId "" -RefObjectId ""
+
+Get-MsolUser -ObjectID UserPrincipalName
+
+Get-AzureADUser -ObjectID UserPrincipalName
+Get-AzureADUserManager -ObjectID UserPrincipalName
---
+++ MFA/gwii_users/mfa_gwii_settings.ps1
@@ -0,0 +1,49 @@
+﻿Get-ExecutionPolicy
+Set-ExecutionPolicy Unrestricted –Scope CurrentUser
+
+#############################################
+######     Import CSV File of users    ######
+######     To update MFA for Accounts  ######
+#############################################
+
+
+
+#Get User Credentials and login to MS Online
+$UserCredential = Get-Credential
+Import-Module MSOnline
+Connect-MsolService –Credential $UserCredential
+
+#List MFA Users
+Get-MsolUser  | Select-Object UserPrincipalName,StrongAuthenticationMethods,StrongAuthenticationRequirements 
+
+#Import DatA from CSV file and create variable
+#Import-Csv ".\mfa_gwii.csv";
+$UsersArray = Import-Csv ".\mfa_gwii.csv";
+#search through file and create array for Data input
+foreach($user in $UsersArray)
+{
+    If ($user.UpdateNow -eq "Yes")
+    {
+        $st = New-Object -TypeName Microsoft.Online.Administration.StrongAuthenticationRequirement
+        $st.RelyingParty = "*"
+        #State can be Enforced, Enabled, Disabled
+        $st.State = "Enforced"
+        $sta = @($st)
+        $m=@()
+        #Take variables and set the state of each user
+        Set-MsolUser -UserPrincipalName $user.UserEmail -StrongAuthenticationRequirements $sta
+        #Make users Re-Register MFA
+        set-msoluser -Userprincipalname $user.UserEmail -StrongAuthenticationMethods $m
+        #Output message stating that Data in Username column has been used to update that account
+        Write-Host $User.Username "has been update with a MFA state of '"$st.State"'" -ForegroundColor Yellow
+     }
+     Else
+     {
+          If($user.UpdateNow -eq "No" -and $user.mfastatus -eq "Enforced")
+          {Write-Host $User.Username "MFA Status = " $User.mfastatus -ForegroundColor Green
+          }
+          Else
+          {Write-Host $User.Username "MFA Status = " $User.mfastatus -ForegroundColor Red
+          }
+     }
+}
